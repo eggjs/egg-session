aabf743e93dbcf612d53436c7ad6f7c0da006d3a
popomore <sakura9515@gmail.com>
2016-11-16 11:57:59 +0800

    feat: check response cookie's length
    Max cookie length is 4093
    
    http://browsercookielimits.squawky.net/

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const compose = require('koa-compose');
+
 module.exports = (options, app) => {
-  return require('koa-session')(options, app);
+  const cookieSessionMatchKey = `${options.key}=`;
+  return compose([
+    function* checkCookieLength(next) {
+      yield next;
+      const setCookie = this.response.headers['set-cookie'];
+      if (!setCookie) {
+        return;
+      }
+      for (const val of setCookie) {
+        // http://browsercookielimits.squawky.net/
+        if (val.indexOf(cookieSessionMatchKey) === 0 && val.length > 4093) {
+          const err = new Error(`Max cookie limit is 4093, but got ${val.length}`);
+          err.name = 'CookieLimitExceedError';
+          err.cookie = val;
+          this.coreLogger.error(err);
+          break;
+        }
+      }
+    },
+    require('koa-session')(options, app),
+  ]);
 };
