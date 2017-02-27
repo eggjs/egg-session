3412b9be0b70d15338d2bb7fc45406a3047af989
Yiyu He <dead-horse@users.noreply.github.com>
2017-02-27 09:56:15 +0800

    feat: remove cookie length check (#6)

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -1,27 +1,3 @@
 'use strict';
 
-const compose = require('koa-compose');
-
-module.exports = (options, app) => {
-  const cookieSessionMatchKey = `${options.key}=`;
-  return compose([
-    function* checkCookieLength(next) {
-      yield next;
-      const setCookie = this.response.headers['set-cookie'];
-      if (!setCookie) {
-        return;
-      }
-      for (const val of setCookie) {
-        // http://browsercookielimits.squawky.net/
-        if (val.indexOf(cookieSessionMatchKey) === 0 && val.length > 4093) {
-          const err = new Error(`Max cookie limit is 4093, but got ${val.length}`);
-          err.name = 'CookieLimitExceedError';
-          err.cookie = val;
-          this.coreLogger.error(err);
-          break;
-        }
-      }
-    },
-    require('koa-session')(options, app),
-  ]);
-};
+module.exports = require('koa-session');
