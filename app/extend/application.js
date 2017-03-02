aafbe066f441fefd31129d82602057d7f581a167
Yiyu He <dead-horse@users.noreply.github.com>
2017-03-02 18:22:20 +0800

    feat: support set sessionStore to null and get sessionStore (#8)

--- app/extend/application.js
+++ app/extend/application.js
@@ -24,10 +24,29 @@ module.exports = {
    * @param  {Class|Object} store session store class or instance
    */
   set sessionStore(store) {
+    if (this.config.session.store && this.config.env !== 'unittest') {
+      this.logger.warn('[egg-session] sessionStore already exists and will be overwrite');
+    }
+
+    // supoprt this.sesionStore = null to disable external store
+    if (!store) {
+      this.config.session.store = undefined;
+      return;
+    }
+
     if (typeof store === 'function') store = new store(this);
     assert(typeof store.get === 'function', 'store.get must be function');
     assert(typeof store.set === 'function', 'store.set must be function');
     assert(typeof store.destroy === 'function', 'store.destroy must be function');
     this.config.session.store = store;
+  },
+
+  /**
+   * get sessionStore
+   *
+   * @return {Object} session store instance
+   */
+  get sessionStore() {
+    return this.config.session.store;
   },
 };
